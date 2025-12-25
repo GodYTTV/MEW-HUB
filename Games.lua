095e19eedca81e012456cffa3e31167fab2aa283
Meowy <104668777+GodYTTV@users.noreply.github.com>
2025-12-25 23:14:46 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,3 +1,6 @@
 return {
-  [212154879] = "https://api.realaya.xyz/v1/files/l/s1hlubq6c2glhn3b9m9pfcaiaaopoozf.lua",
+  [128451689942376] = "https://api.realaya.xyz/v1/files/l/pvp613pqsxmsiz2uavgjo0a2ol6696g6.lua",
+  [18337464872] = "https://api.realaya.xyz/v1/files/l/t4boosbheqe5e9strlo84yjn7z7ewran.lua",
+  [100744519298647] = "https://api.realaya.xyz/v1/files/l/rd54ulikji7guuyx2totkncg0lshlp5k.lua",
+  [16695366828] = "https://api.realaya.xyz/v1/files/l/tf4z8891w9ywz7q36vpcypjv3d9xwxlv.lua",
 }
