80762dbf1e8d98cfccfee5289a2cbd8cbcf64cd8
Meowy <104668777+GodYTTV@users.noreply.github.com>
2025-12-25 20:52:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,15 +1,14 @@
-local Games = {
-  [1] = "",
-}
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/GodYTTV/MEW-HUB/refs/heads/main/Games.lua"))()
 
 local PlaceId = game.PlaceId
+local GameId = game.GameId
 
-local Script = Games[PlaceId]
+local Script = (Games[PlaceId] or Games[GameId]) or nil
 
 if Script then
-  loadstring(game:HttpGet(Script))()
+    loadstring(game:HttpGet(Script))()
 else
-  game:GetService("Players").LocalPlayer:Kick("\nMew HUB\nGame Not Support\nAuto Close In 5 Seconds")
-  task.wait(5.1)
-  game:Shutdown()
+    game:GetService("Players").LocalPlayer:Kick("\nMew HUB\nGame Not Support\nAuto Close In 5 Seconds")
+    task.wait(5.1)
+    game:Shutdown()
 end
